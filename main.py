92b226cef66f2da5d90fcbf72e1bd50dba11c4c5
Gwizee <risingfire69@gmail.com>
2024-11-28 21:31:02 +0600

    Weather condition Activity has completed

---
+++ main.py
@@ -0,0 +1,6 @@
+def display_weather(atumn,spring) :
+    print("The weather of atumn is: ",atumn)
+    print("The weather of spring is: ",spring)
+atumn_condition = "Weather is really good!"
+spring_condition = "Spring is very plesent!"
+display_weather(atumn_condition,spring_condition)
